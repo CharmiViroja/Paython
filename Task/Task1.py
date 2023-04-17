de882a18bdc45d570ee2f892ae69d9ff1c655a50
CharmiViroja <charmiamrutiya@gmail.com>
2023-04-17 10:14:50 +0530

    17-04-23

---
+++ Task/Task1.py
@@ -0,0 +1,13 @@
+for i in range (1,6):
+    num = int(input("Enter number : "))
+    if num % 2==0:
+        print ("Even")
+    else:
+        print("Odd ")
+
+for i in range (1,6):
+    num = int(input("Enter number : "))
+    if num < 0:
+        print ("Negative")
+    else:
+        print("Positive")
